a3ff4a28c163fabb2b69c7851100f87878156a68
kevin8958 <kevin8958@gmail.com>
2025-07-24 01:31:05 +0900

    feat: add text and cardswap on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,49 @@
+import Typography from '@/components/common/Typography';
 import Squares from '@/components/interaction/Squares';
+import CardSwap, { Card } from '@/components/interaction/CardSwap2';
+
 export default function Index() {
   return (
     <div className="flex h-[calc(100dvh)] w-full items-center justify-center text-white">
-      <Squares
-        speed={0.5}
-        squareSize={40}
-        direction="diagonal" // up, down, left, right, diagonal
-        borderColor="#2F344C"
-        hoverFillColor="#0A0E19"
-      />
+      <div className="fixed z-10 size-full">
+        <Squares
+          speed={0.5}
+          squareSize={40}
+          direction="diagonal" // up, down, left, right, diagonal
+          borderColor="#2F344C"
+          hoverFillColor="#0A0E19"
+        />
+      </div>
+      <div className="realative z-20 w-full px-20">
+        <div className="flex w-full items-center justify-between">
+          <div className="flex flex-col items-start">
+            <Typography variant="h4" classes="opacity-60 pl-1">
+              Frontend developer
+            </Typography>
+            <Typography variant="h2">Kevin</Typography>
+            <Typography variant="h4" classes="opacity-40 mt-4">
+              Crafting interactive, user-focused web experiences
+              <br /> with clean and modern code.
+            </Typography>
+          </div>
+          <div className="relative h-[400px]">
+            <CardSwap cardDistance={60} verticalDistance={70} delay={5000} pauseOnHover={false}>
+              <Card customClass="p-6">
+                <h3>Card 1</h3>
+                <p>Your content here</p>
+              </Card>
+              <Card customClass="p-6">
+                <h3>Card 2</h3>
+                <p>Your content here</p>
+              </Card>
+              <Card customClass="p-6">
+                <h3>Card 3</h3>
+                <p>Your content here</p>
+              </Card>
+            </CardSwap>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/layout/Gnb.tsx
+++ src/components/layout/Gnb.tsx
@@ -8,11 +8,11 @@ const Gnb = () => {
   const router = useRouter();
   return (
     <div className="bg-primary-990 border-primary-100/30 fixed top-6 left-1/2 z-50 flex w-fit -translate-x-1/2 items-center justify-center rounded-2xl border border-[1px] px-4 py-2 transition-all duration-500 ease-in-out">
-      <Button classes="min-w-[80px]" variant="clear" size="sm" onClick={() => router.push('/')}>
+      <Button variant="clear" size="sm" onClick={() => router.push('/')}>
         Home
       </Button>
-      <Button classes="min-w-[80px]" variant="clear" size="sm" onClick={() => router.push('/')}>
-        About
+      <Button variant="clear" size="sm" onClick={() => router.push('/')}>
+        Projects
       </Button>
       <Button
         classes="min-w-[80px]"
@@ -22,7 +22,7 @@ const Gnb = () => {
       >
         Components
       </Button>
-      <Button classes="min-w-[80px]" variant="clear" size="sm" onClick={() => router.push('/')}>
+      <Button variant="clear" size="sm" onClick={() => router.push('/')}>
         Contact
       </Button>
     </div>
